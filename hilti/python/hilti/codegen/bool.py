11b172a96b7cca314804133cf5d5515a33cedb66
Robin Sommer <robin@icir.org>
2009-01-30 00:51:10 +0000

    Checkpoint

---
+++ hilti/python/hilti/codegen/bool.py
@@ -0,0 +1,14 @@
+# $Id$
+#
+# Code generator for bool instructions.
+
+import llvm.core
+
+from hilti.core import *
+from hilti import instructions
+from codegen import codegen
+
+@codegen.convertToLLVM(type.BoolType)
+def _(type):
+    return llvm.core.Type.int(1)
+
