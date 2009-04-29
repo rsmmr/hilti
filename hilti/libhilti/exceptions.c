b38beaaf10e8265d5c8e757d8e67b4c718575162
Matthias Vallentin <vallentin@ICSI.Berkeley.EDU>
2009-04-29 21:31:38 +0000

    An off-by-one in exeception handling code caused NUL bytes to appear in output. As Robin noted, that's why the diffs were sometimes treated as binary files.

--- hilti/libhilti/exceptions.c
+++ hilti/libhilti/exceptions.c
@@ -28,7 +28,7 @@ void __hlt_exception_print_uncaught(__hlt_exception exception)
 {
     const char* name = (const char*)exception; 
 
-    write(2, "hilti: uncaught exception, ", 28);
+    write(2, "hilti: uncaught exception, ", 27);
     write(2, name, strlen(name));
     write(2, "\n", 1);
 }
