d38606ce7f49f3283b4146059f6efdfee785a21d
Robin Sommer <robin@icir.org>
2009-09-17 23:33:13 +0000

    Macros in preparation for transition to CMake

---
+++ cmake/EnableLLVM.cmake
@@ -0,0 +1,25 @@
+# $Id$
+#
+# If ENABLE_LLVM is defined, we change the compiler setting to compile and 
+# link LLVM bitcode.
+
+if ( ENABLE_LLVM )
+   if ( LLVM_FOUND )
+       message("building LLVM bitcode files")
+       
+       set(CMAKE_C_OUTPUT_EXTENSION ".bc")
+       set(CMAKE_STATIC_LIBRARY_SUFFIX ".bca")
+       set(CMAKE_C_COMPILER "llvm-gcc")
+       set(CMAKE_C_FLAGS "-emit-llvm ${CMAKE_C_FLAGS} ${LLVM_COMPILE_FLAGS}")
+       set(CMAKE_AR "llvm-ar")
+       set(CMAKE_RANLIB "llvm-ranlib")
+       
+   else ( LLVM_FOUND )
+       message(FATAL_ERROR "LLVM build requested but LLVM installation not found.")
+       
+   endif ( LLVM_FOUND )
+   
+else ( ENABLE_LLVM )
+       message("building native object files")
+
+endif ( ENABLE_LLVM )
