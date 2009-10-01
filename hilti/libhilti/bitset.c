6cf471cf85bb5fcc690bc87eaff0869b3b894f78
Robin Sommer <robin@icir.org>
2009-10-01 16:13:58 +0000

    Adding missing files

---
+++ hilti/libhilti/bitset.c
@@ -0,0 +1,47 @@
+/* $Id$
+ * 
+ * Support functions HILTI's bitset data type.
+ * 
+ */
+
+#include "hilti.h"
+
+#include <stdio.h>
+
+static hlt_string_constant _sep = { 1, "|" };
+static hlt_string_constant _none = { 7, "<empty>" };
+
+hlt_string hlt_bitset_to_string(const hlt_type_info* type, const void* obj, int32_t options, hlt_exception* excpt)
+{
+    assert(type->type == HLT_TYPE_BITSET);
+    int64_t i = *((int64_t*)obj);
+
+    // Labels are stored as concatenated ASCIIZ. 
+    const char *labels = (const char*) type->aux;
+    
+    hlt_string str = 0;
+    int bit = 1;
+    while ( *labels ) {
+        if ( i & bit ) {
+            if ( ! str )
+                str = hlt_string_from_asciiz(labels, excpt);
+            else {
+                str = hlt_string_concat(str, &_sep, excpt);
+                str = hlt_string_concat(str, hlt_string_from_asciiz(labels, excpt), excpt);
+            }
+
+        }
+        bit <<= 1;
+        while ( *labels++ );
+    }
+            
+    return str ? str : &_none;
+}
+
+int64_t hlt_bitset_to_int64(const hlt_type_info* type, const void* obj, hlt_exception* expt)
+{
+    assert(type->type == HLT_TYPE_BITSET);
+    return *((int64_t*)obj);
+}
+
+
---
+++ hilti/libhilti/bitset.h
@@ -0,0 +1,13 @@
+// $Id$
+// 
+// Support functions for HILTI's bitset data type.
+
+#ifndef HILTI_BITSET_H
+#define HILTI_BITSET_H
+
+#include "exceptions.h"
+
+extern hlt_string hlt_bitset_to_string(const hlt_type_info* type, const void* obj, int32_t options, hlt_exception* excpt);
+extern int64_t hlt_bitset_to_int64(const hlt_type_info* type, const void* obj, hlt_exception* expt);
+
+#endif
