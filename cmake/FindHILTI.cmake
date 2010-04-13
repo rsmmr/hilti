7fc28a328e5858d9af232b73ff9bdbcd0e8c56f5
Robin Sommer <robin@icir.org>
2010-04-13 21:29:56 +0000

    Missing file

---
+++ cmake/FindHILTI.cmake
@@ -0,0 +1,24 @@
+# Detect HILTI and set various variables.
+#
+# HILTI_CFLAGS    : compile flags needed to build a C program using HILTI headers.
+# HILTI_LDFLAGS   : ldflags needed to link
+
+find_program(HILTI_CONFIG_EXECUTABLE
+      NAMES hilti-config
+      PATHS
+      /opt/local/bin
+  )
+
+exec_program(${HILTI_CONFIG_EXECUTABLE} ARGS --cflags  OUTPUT_VARIABLE HILTI_CFLAGS)
+exec_program(${HILTI_CONFIG_EXECUTABLE} ARGS --ldflags OUTPUT_VARIABLE HILTI_LDFLAGS)
+
+if(HILTI_CONFIG_EXECUTABLE)
+    set(HILTI_FOUND TRUE)
+endif(HILTI_CONFIG_EXECUTABLE)
+  
+if(HILTI_FOUND)
+    message(STATUS "Found hilti-config: ${HILTI_CONFIG_EXECUTABLE}")
+else(HILTI_FOUND)
+    message(FATAL_ERROR "Could NOT find hilti-config")
+endif(HILTI_FOUND)
+
