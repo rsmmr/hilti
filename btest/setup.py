22ea593ff74454c8705385821909622ee27316c2
Robin Sommer <robin@icir.org>
2011-07-23 13:18:38 -0700

    Updating btest to 0.22-18.
    Note that btest is now maintained as part of Bro. However, we keep a
    copy here to make our life easier.

--- btest/setup.py
+++ btest/setup.py
@@ -3,11 +3,11 @@
 from distutils.core import setup
 
 setup(name='btest',
-      version="0.21",
+      version="0.22",
       description='A simple unit testing framework',
       author='Robin Sommer',
       author_email='robin@icir.org',
       url='http://www.icir.org/robin/btest',
-      scripts=['btest', 'btest-diff'],
+      scripts=['btest', 'btest-diff', "btest-bg-run", "btest-bg-run-helper", "btest-bg-wait", "btest-setsid"],
       include_dirs=["examples", "Baseline"]
      )
