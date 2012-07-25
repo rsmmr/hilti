6e2860fc8dfeca4c1e8629163205b8c03b7cd9af
Robin Sommer <robin@icir.org>
2012-07-24 18:21:52 -0700

    Fixing bytes test.

--- tests/c-api/bytes.c
+++ tests/c-api/bytes.c
@@ -38,8 +38,10 @@ void printc(const int8_t* c, int len)
 
 const char* myexp(const hlt_exception* e)
 {
-    if ( e )
+    if ( e ) {
+        GC_DTOR(e, hlt_exception);
         return "excepetion";
+    }
     else
         return "no-exception";
 }
@@ -102,89 +104,101 @@ int main()
     hlt_bytes* sub = hlt_bytes_sub(p, p2, &e, 0);
     printb(sub);
     printf("A len = %ld (4) %s\n", hlt_bytes_len(sub, &e, 0), myexp(e));
-
     printf("-----\n");
+    GC_DTOR(sub, hlt_bytes);
 
-    sub = hlt_bytes_sub(hlt_bytes_begin(b, &e, 0), p2, &e, 0);
+    hlt_iterator_bytes bb = hlt_bytes_begin(b, &e, 0);
+    hlt_iterator_bytes be = hlt_bytes_end(b, &e, 0);
+
+    sub = hlt_bytes_sub(bb, p2, &e, 0);
     printb(sub);
     printf("B len = %ld (7) %s\n", hlt_bytes_len(sub, &e, 0), myexp(e));
-
     printf("-----\n");
     GC_DTOR(sub, hlt_bytes);
 
-    sub = hlt_bytes_sub(p2, hlt_bytes_end(b, &e, 0), &e, 0);
+    sub = hlt_bytes_sub(p2, be, &e, 0);
     printb(sub);
     printf("C len = %ld (2051) %s\n", hlt_bytes_len(sub, &e, 0), myexp(e));
-
     printf("-----\n");
     GC_DTOR(sub, hlt_bytes);
 
-    sub = hlt_bytes_sub(hlt_bytes_begin(b, &e, 0), hlt_bytes_end(b, &e, 0), &e, 0);
+    sub = hlt_bytes_sub(bb, be, &e, 0);
     printb(sub);
     printf("D len = %ld (%ld) %s\n", hlt_bytes_len(sub, &e, 0), hlt_bytes_len(b, &e, 0), myexp(e));
-
     printf("-----\n");
     GC_DTOR(sub, hlt_bytes);
 
     GC_DTOR(p, hlt_bytes);
+    GC_DTOR(p2, hlt_bytes);
+
+//X
 
     p = hlt_bytes_offset(b2, 3, &e, 0);
     p2 = hlt_bytes_offset(b2, 1500, &e, 0);
     sub = hlt_bytes_sub(p, p2, &e, 0);
     printb(sub);
     printf("E len = %ld (1497) %s\n", hlt_bytes_len(sub, &e, 0), myexp(e));
-
     printf("-----\n");
+    GC_DTOR(p, hlt_bytes);
+    GC_DTOR(p2, hlt_bytes);
+    GC_DTOR(sub, hlt_bytes);
 
     ///
 
     p = hlt_bytes_offset(b, 3, &e, 0);
     p2 = hlt_bytes_offset(b, 7, &e, 0);
-    const int8_t* rsub = hlt_bytes_sub_raw_copy(p, p2, &e, 0);
+    int8_t* rsub = hlt_bytes_sub_raw(p, p2, &e, 0);
     hlt_bytes_size d = hlt_iterator_bytes_diff(p, p2, &e, 0);
     printc(rsub, d);
-    printf("A len = %ld (4) %s\n", hlt_bytes_len(sub, &e, 0), myexp(e));
-
+    printf("A len = %ld (4) %s\n", d, myexp(e));
     printf("-----\n");
+    hlt_free(rsub);
 
-    rsub = hlt_bytes_sub_raw_copy(hlt_bytes_begin(b, &e, 0), p2, &e, 0);
-    d = hlt_iterator_bytes_diff(hlt_bytes_begin(b, &e, 0), p2, &e, 0);
+    rsub = hlt_bytes_sub_raw(bb, p2, &e, 0);
+    d = hlt_iterator_bytes_diff(bb, p2, &e, 0);
     printc(rsub, d);
     printf("B len = %ld (7) %s\n", d, myexp(e));
-
     printf("-----\n");
+    hlt_free(rsub);
 
-    rsub = hlt_bytes_sub_raw_copy(p2, hlt_bytes_end(b, &e, 0), &e, 0);
-    d = hlt_iterator_bytes_diff(p2, hlt_bytes_end(b, &e, 0), &e, 0);
+    rsub = hlt_bytes_sub_raw(p2, be, &e, 0);
+    d = hlt_iterator_bytes_diff(p2, be, &e, 0);
     printc(rsub, d);
     printf("C len = %ld (2051) %s\n", d, myexp(e));
-
     printf("-----\n");
+    hlt_free(rsub);
 
-
-    rsub = hlt_bytes_sub_raw_copy(hlt_bytes_begin(b, &e, 0), hlt_bytes_end(b, &e, 0), &e, 0);
-    d = hlt_iterator_bytes_diff(hlt_bytes_begin(b, &e, 0), hlt_bytes_end(b, &e, 0), &e, 0);
+    rsub = hlt_bytes_sub_raw(bb, be, &e, 0);
+    d = hlt_iterator_bytes_diff(bb, be, &e, 0);
     printc(rsub, d);
     printf("D len = %ld (%ld) %s\n", d, hlt_bytes_len(b, &e, 0), myexp(e));
+    hlt_free(rsub);
 
     printf("-----\n");
     GC_DTOR(p, hlt_iterator_bytes);
     GC_DTOR(p2, hlt_iterator_bytes);
+
+//X
 
     p = hlt_bytes_offset(b2, 3, &e, 0);
     p2 = hlt_bytes_offset(b2, 1500, &e, 0);
-    rsub = hlt_bytes_sub_raw_copy(p, p2, &e, 0);
+    rsub = hlt_bytes_sub_raw(p, p2, &e, 0);
     d = hlt_iterator_bytes_diff(p, p2, &e, 0);
     printc(rsub, d);
     printf("E len = %ld (1497) %s\n", d, myexp(e));
-
     printf("-----\n");
-
-    GC_DTOR(rsub, hlt_bytes);
+    hlt_free(rsub);
     GC_DTOR(p, hlt_iterator_bytes);
     GC_DTOR(p2, hlt_iterator_bytes);
 
+    GC_DTOR(b, hlt_bytes);
+    GC_DTOR(b2, hlt_bytes);
+    GC_DTOR(bb, hlt_iterator_bytes);
+    GC_DTOR(be, hlt_iterator_bytes);
+
     ///
+
+//X
 
     e = 0;
     b = hlt_bytes_new(&e, 0);
@@ -194,20 +208,31 @@ int main()
         hlt_bytes_append_raw_copy(b, large + i, 1, &e, 0);
 
     p = hlt_bytes_begin(b, &e, 0);
-    for ( ; ! hlt_iterator_bytes_eq(p, hlt_bytes_end(b, &e, 0), &e, 0); p = hlt_iterator_bytes_incr(p, &e, 0) ) {
+    be = hlt_bytes_end(b, &e, 0);
+
+    while ( ! hlt_iterator_bytes_eq(p, be, &e, 0) ) {
         int8_t u = hlt_iterator_bytes_deref(p, &e, 0);
         sum2 += u;
+
+        hlt_iterator_bytes t = p;
+        p = hlt_iterator_bytes_incr(p, &e, 0);
+        GC_DTOR(t, hlt_iterator_bytes);
     }
+
+    GC_DTOR(p, hlt_iterator_bytes);
+    GC_DTOR(be, hlt_iterator_bytes);
 
     printf("sum2 = %d (%d) %s\n", sum2, sum, myexp(e));
 
     p = hlt_bytes_offset(b, 0, &e, 0);
     p2 = hlt_bytes_offset(b, -1, &e, 0);
     printf("diff = %ld (2047) %s\n", hlt_iterator_bytes_diff(p, p2, &e, 0), myexp(e));
+    GC_DTOR(p, hlt_iterator_bytes);
+    GC_DTOR(p2, hlt_iterator_bytes);
 
     GC_DTOR(b, hlt_bytes);
 
-    ///
+///X
 
     e = 0;
     b = hlt_bytes_new(&e, 0);
@@ -217,45 +242,64 @@ int main()
         hlt_bytes_append_raw_copy(b, large, LEN, &e, 0);
 
     p = hlt_bytes_begin(b, &e, 0);
-    for ( ; ! hlt_iterator_bytes_eq(p, hlt_bytes_end(b, &e, 0), &e, 0); p = hlt_iterator_bytes_incr(p, &e, 0) ) {
+    be = hlt_bytes_end(b, &e, 0);
+
+    while ( ! hlt_iterator_bytes_eq(p, be, &e, 0) ) {
         int8_t u = hlt_iterator_bytes_deref(p, &e, 0);
         sum2 += u;
+
+        hlt_iterator_bytes t = p;
+        p = hlt_iterator_bytes_incr(p, &e, 0);
+        GC_DTOR(t, hlt_iterator_bytes);
     }
 
-    printf("sum2 = %d (%d) %s\n", sum2, sum * 10, myexp(e));
-
     GC_DTOR(p, hlt_iterator_bytes);
+    GC_DTOR(be, hlt_iterator_bytes);
+
+    printf("sum2 = %d (%d) %s\n", sum2, sum * 10, myexp(e));
 
     p = hlt_bytes_offset(b, 0, &e, 0);
     p2 = hlt_bytes_offset(b, -1, &e, 0);
     printf("diff = %ld (%d) %s\n", hlt_iterator_bytes_diff(p, p2, &e, 0), LEN * 10 - 1, myexp(e));
-
     GC_DTOR(p, hlt_iterator_bytes);
     GC_DTOR(p2, hlt_iterator_bytes);
 
-    ///
+
+///XX
 
     p = hlt_bytes_begin(b, &e, 0);
-    printf("eq = %d (1) %s\n", hlt_iterator_bytes_eq(p, hlt_bytes_begin(b, &e, 0), &e, 0), myexp(e));
-    printf("eq = %d (0) %s\n", hlt_iterator_bytes_eq(p, hlt_bytes_end(b, &e, 0), &e, 0), myexp(e));
-    GC_DTOR(p, hlt_iterator_bytes);
+    bb = hlt_bytes_begin(b, &e, 0);
+    be = hlt_bytes_end(b, &e, 0);
 
+    printf("eq = %d (1) %s\n", hlt_iterator_bytes_eq(p, bb, &e, 0), myexp(e));
+    printf("eq = %d (0) %s\n", hlt_iterator_bytes_eq(p, be, &e, 0), myexp(e));
+
+    hlt_iterator_bytes t = p;
     p = hlt_iterator_bytes_incr(p, &e, 0);
-    printf("eq = %d (0) %s\n", hlt_iterator_bytes_eq(p, hlt_bytes_begin(b, &e, 0), &e, 0), myexp(e));
+    printf("eq = %d (0) %s\n", hlt_iterator_bytes_eq(p, bb, &e, 0), myexp(e));
+    GC_DTOR(t, hlt_iterator_bytes);
+    GC_DTOR(p, hlt_iterator_bytes);
 
     p2 = hlt_bytes_end(b, &e, 0);
-    printf("eq = %d (0) %s\n", hlt_iterator_bytes_eq(p2, hlt_bytes_begin(b, &e, 0), &e, 0), myexp(e));
-    printf("eq = %d (1) %s\n", hlt_iterator_bytes_eq(p2, hlt_bytes_end(b, &e, 0), &e, 0), myexp(e));
+    printf("eq = %d (0) %s\n", hlt_iterator_bytes_eq(p2, bb, &e, 0), myexp(e));
+    printf("eq = %d (1) %s\n", hlt_iterator_bytes_eq(p2, be, &e, 0), myexp(e));
 
-    GC_DTOR(p, hlt_iterator_bytes);
     GC_DTOR(p2, hlt_iterator_bytes);
 
     p = hlt_bytes_offset(b, 0, &e, 0);
-    printf("eq = %d (1) %s\n", hlt_iterator_bytes_eq(p, hlt_bytes_begin(b, &e, 0), &e, 0), myexp(e));
+    printf("eq = %d (1) %s\n", hlt_iterator_bytes_eq(p, bb, &e, 0), myexp(e));
+    GC_DTOR(p, hlt_iterator_bytes);
 
     p = hlt_bytes_offset(b, -1, &e, 0);
+    t = p;
     p = hlt_iterator_bytes_incr(p, &e, 0);
-    printf("eq = %d (1) %s\n", hlt_iterator_bytes_eq(p, hlt_bytes_end(b, &e, 0), &e, 0), myexp(e));
+    printf("eq = %d (1) %s\n", hlt_iterator_bytes_eq(p, be, &e, 0), myexp(e));
+    GC_DTOR(t, hlt_iterator_bytes);
+    GC_DTOR(p, hlt_iterator_bytes);
+
+    GC_DTOR(bb, hlt_iterator_bytes);
+    GC_DTOR(be, hlt_iterator_bytes);
+    GC_DTOR(b, hlt_bytes);
 
     hlt_free(large);
 
