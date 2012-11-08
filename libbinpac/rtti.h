e14db964b8c27f133f037c16af2ce9e588efde61
Robin Sommer <robin@icir.org>
2012-11-07 17:05:01 -0800

    Missing file.

---
+++ libbinpac/rtti.h
@@ -0,0 +1,13 @@
+
+#ifndef LIBBINPAC_RTTI_H
+#define LIBBINPAC_RTTI_H
+
+#include <libhilti/rtti.h>
+
+// Define type constants for HILTI's RTTI system.
+
+#define HLT_TYPE_BINPAC_FILTER      (HLT_TYPE_EXTERN_BINPAC + 1)
+#define HLT_TYPE_BINPAC_SINK        (HLT_TYPE_EXTERN_BINPAC + 2)
+#define HLT_TYPE_BINPAC_MIME_PARSER (HLT_TYPE_EXTERN_BINPAC + 3)
+
+#endif
