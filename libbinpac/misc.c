685213b3be8e0667825405cac4f639b1282494e9
Robin Sommer <robin@icir.org>
2014-08-13 13:07:26 -0700

    Adding a built-in mktime() function for BinPAC++.

--- libbinpac/misc.c
+++ libbinpac/misc.c
@@ -21,3 +21,23 @@ hlt_bytes* binpac_fmt_bytes(hlt_bytes* fmt, const hlt_type_info* type, const voi
 
     return bresult;
 }
+
+hlt_time binpac_mktime(int64_t y, int64_t m, int64_t d, int64_t H, int64_t M, int64_t S, hlt_exception** excpt, hlt_execution_context* ctx)
+{
+    struct tm t;
+    t.tm_sec = S;
+    t.tm_min = M;
+    t.tm_hour = H;
+    t.tm_mday = d;
+    t.tm_mon = m - 1;
+    t.tm_year = y - 1900;
+
+    time_t teatime = mktime(&t);
+
+    if ( teatime >= 0 )
+        return hlt_time_value(teatime, 0);
+
+    hlt_set_exception(excpt, &hlt_exception_value_error, 0);
+    return 0;
+}
+
