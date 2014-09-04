f715a8db91b4ba5c350c7794178855b2055fb8e3
Robin Sommer <robin@icir.org>
2014-09-03 20:45:29 -0700

    Moving justrx into libhilti folder.

---
+++ libhilti/justrx/src/jlocale.h
@@ -0,0 +1,31 @@
+// $Id$
+//
+// Function implementing local- and encoding-specific functionality.
+//
+// TODO: Currently, these are just hard-coded in local-independent,
+// ASCII-only way.
+
+#ifndef JRX_JITTYPE_H
+#define JRX_JITTYPE_H
+
+#include <ctype.h>
+
+#include "jrx-intern.h"
+#include "ccl.h"
+
+extern jrx_ccl* local_ccl_lower(jrx_ccl_group* set);
+extern jrx_ccl* local_ccl_upper(jrx_ccl_group* set);
+extern jrx_ccl* local_ccl_word(jrx_ccl_group* set);
+extern jrx_ccl* local_ccl_digit(jrx_ccl_group* set);
+extern jrx_ccl* local_ccl_blank(jrx_ccl_group* set);
+
+static inline int _isword(jrx_char cp) {
+    return isalnum(cp) || cp == '_';
+}
+
+static inline int local_word_boundary(jrx_char* prev, jrx_char current)
+{
+    return _isword(current) ? (prev ? ! _isword(*prev) : 1) : 0;
+}
+
+#endif
