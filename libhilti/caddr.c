67b330303e16922808577f4314aa1e2af4993eea
Robin Sommer <robin@icir.org>
2015-02-15 21:48:44 -0800

    Test updates.
    Pass now (mostly) on Mac OS as well.

--- libhilti/caddr.c
+++ libhilti/caddr.c
@@ -10,8 +10,15 @@ hlt_string hlt_caddr_to_string(const hlt_type_info* type, const void* obj, int32
 {
     assert(type->type == HLT_TYPE_CADDR);
 
+    void* p = *((void**)obj);
+
     char buffer[32];
-    snprintf(buffer, 32, "addr:%p", *((void**)obj));
+
+    if ( p )
+        snprintf(buffer, 32, "addr:%p", p);
+    else
+        snprintf(buffer, 32, "addr:(nil)");
+
     buffer[31] = '\0';
 
     return hlt_string_from_asciiz(buffer, exception, ctx);
